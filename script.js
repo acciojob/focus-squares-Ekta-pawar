a15777428c84f2b1f6c5851b5cf5197d12378638
Ekta-pawar <ektapawar0001@gmail.com>
2025-07-17 19:41:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const squares = document.querySelectorAll('.square');
+//your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+const square1 = document.getElementById('square1');
+const square2 = document.getElementById('square2'); 
+const square3 = document.getElementById('square3');
 
-  squares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-      squares.forEach((otherSquare) => {
-        if (otherSquare !== square) {
-          otherSquare.style.backgroundColor = '#6F4E37'; // Coffee
-        }
-      });
-    });
+const originalColor = square1.style.backgroundColor;
 
-    square.addEventListener('mouseleave', () => {
-      squares.forEach((otherSquare) => {
-        if (otherSquare !== square) {
-          otherSquare.style.backgroundColor = '#E6E6FA'; // Lavender
-        }
-      });
-    });
+squares.forEach(function(sqr) {
+  sqr.addEventListener('mouseover', (e) => {
+    if (e.target.id === 'square1') {
+      square2.style.backgroundColor = '#6F4E37';
+      square3.style.backgroundColor = '#6F4E37';
+    }else if (e.target.id === 'square2') {
+      square1.style.backgroundColor = '#6F4E37';
+      square3.style.backgroundColor = '#6F4E37';
+    } else if (e.target.id === 'square3') {
+      square1.style.backgroundColor = '#6F4E37';
+      square2.style.backgroundColor = '#6F4E37';
+    }
+  });
+
+  sqr.addEventListener('mouseout', (e) => {
+    if (e.target.id === 'square1') {
+      square2.style.backgroundColor = originalColor;
+      square3.style.backgroundColor = originalColor;
+    }else if (e.target.id === 'square2') {
+      square1.style.backgroundColor = originalColor;
+      square3.style.backgroundColor = originalColor;
+    } else if (e.target.id === 'square3') {
+      square1.style.backgroundColor = originalColor;
+      square2.style.backgroundColor = originalColor;
+    }
   });
 });
+
